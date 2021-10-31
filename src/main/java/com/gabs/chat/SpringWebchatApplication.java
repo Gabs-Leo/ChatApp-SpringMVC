9d95bb446b35648831e3ef20ceef453bf6734e7c
GabsLeo <gabrieldemoraistaveira@gmail.com>
2021-10-31 15:40:01 -0300

    :tada: First Commit

---
+++ src/main/java/com/gabs/chat/SpringWebchatApplication.java
@@ -0,0 +1,13 @@
+package com.gabs.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebchatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebchatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabs/chat/SpringWebchatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabs.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebchatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
